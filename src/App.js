7ddcf3d602d272396738a52d0a78ab61346ddfc7
Miriam Ertl <miriam_ertl@web.de>
2023-08-25 15:54:41 +0200

    add List.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import Form from "./components/Form/Form.js";
+import List from "./components/List/List.js";
 import { nanoid } from "nanoid";
 
 function App() {
-  const [activities, setActivities] = useState();
+  const [activities, setActivities] = useLocalStorageState("activity", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { ...newActivity, id: nanoid(6) }]);
   }
+
   return (
     <div>
+      <List activties={activities} />
       <Form onAddActivity={handleAddActivity} />
     </div>
   );

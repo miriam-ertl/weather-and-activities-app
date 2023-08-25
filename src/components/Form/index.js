c3448346a9b8d8b0a7e23ad3bbf3aca2a0deeace
Tien Pham <tienphamvanfuv@gmail.com>
2023-08-25 11:23:03 +0200

    add form component

---
+++ src/components/Form/index.js
@@ -0,0 +1,18 @@
+export default function Form() {
+  return (
+    <form>
+      <h1>Weather actitity</h1>
+      <div>
+        <label>Name 1</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Name 2</label>
+        <input></input>
+      </div>
+      <div>
+        <button>Submit></button>
+      </div>
+    </form>
+  );
+}

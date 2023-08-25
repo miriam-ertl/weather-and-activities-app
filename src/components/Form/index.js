d5ac08381754ee422c802ce0c4bded797ca654ac
Tien Pham <tienphamvanfuv@gmail.com>
2023-08-25 11:39:19 +0200

    add className and type

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,17 @@
 export default function Form() {
   return (
     <form>
-      <h1>Weather actitity</h1>
-      <div>
-        <label>Name 1</label>
-        <input></input>
+      <h1>Add new actitity:</h1>
+      <div classNmae="form__field">
+        <label htmlFor="activityName">Name of activity:</label>
+        <input type="text" name="activityName" id="activityName" />
+      </div>
+      <div className="form__checkbox">
+        <label htmlFor="activityBox">Good weather actitity:</label>
+        <input type="checkbox" name="activityBox" id="activityBox"></input>
       </div>
       <div>
-        <label>Name 2</label>
-        <input></input>
-      </div>
-      <div>
-        <button>Submit></button>
+        <button>Submit</button>
       </div>
     </form>
   );

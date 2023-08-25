5bb640c9591e0009dc0d4637776b99a39624d525
Miriam Ertl <miriam_ertl@web.de>
2023-08-25 15:05:49 +0200

    add handleAddActivity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import Form from "./components/Form/Form.js";
+import { nanoid } from "nanoid";
+
 function App() {
-  return <div>Hello World</div>;
+  const [activities, setActivities] = useState();
+
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { ...newActivity, id: nanoid(6) }]);
+  }
+  return (
+    <div>
+      <Form onAddActivity={handleAddActivity} />
+    </div>
+  );
 }
 
 export default App;
